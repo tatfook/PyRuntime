71e8a32021a7bc55ebf2c0e840c44ce4fe5b72fb
DreamAndDead <favorofife@yeah.net>
2019-10-14 10:36:24 +0800

    return error message

--- Transpiler.lua
+++ Transpiler.lua
@@ -6,31 +6,44 @@ Desc: Call external py2lua.exe to tranpile pycode to luacode
 use the lib:
 ------------------------------------------------------------
 local transpiler = NPL.load("Mod/PyTranspiler/Transpiler.lua")
-luacode = transpiler:transpile(pycode)
-
--- TODO:
--- error handles
-
+local luacode, error_msg = transpiler:transpile(pycode)
+if luacode == nil then
+    -- handle error_msg
+end
 ------------------------------------------------------------
 --]]
 
 local Transpiler = NPL.export()
 
+--[[
+    params:
+    - pycode
+    returns:
+    - luacode
+    - error_msg
+]]
 function Transpiler:transpile(pycode)
-    if(not Transpiler.OsSupported())then
-	    LOG.std(nil, "info", "PyTranspiler", "PyTranspiler transpiler isn't supported on %s", System.os.GetPlatform())
-        return
+    if not Transpiler:OsSupported() then
+        local error_msg = "PyTranspiler Mod isn't supported on " .. System.os.GetPlatform() .. " platform"
+        return nil, error_msg
     end
 
-    -- TODO: some more error handling
     local app_root = ParaIO.GetCurDirectory(0)
+    local py2lua_exe = app_root .. "plugins/py2lua.exe"
 
-    -- TODO: exe exist?
-    local luacode = ParaGlobal.ExecuteFilter(app_root .. "plugins/py2lua.exe", pycode)
+    if not ParaIO.DoesFileExist(py2lua_exe) then
+        return nil, "py2lua.exe not found in plugins directory"
+    end
+
+    if not ParaGlobal.ExecuteFilter then
+        return nil, "ParaGlobal.ExecuteFilter not found in NPL Runtime"
+    end
+
+    local luacode = ParaGlobal.ExecuteFilter(py2lua_exe, pycode)
     return luacode
 end
 
 function Transpiler:OsSupported()
-    local is_supported = (System.os.GetPlatform()=="win32")
+    local is_supported = (System.os.GetPlatform() == "win32")
     return is_supported;
 end
--- readme.md
+++ readme.md
@@ -1,2 +1,46 @@
 # PyTranspiler Mod
 
+一个 npl mod，用来将可执行的 python 代码转换为 lua 代码。
+
+## 支持语言版本
+
+针对 python 3 版本，支持大部分 features，具体可参考 https://github.com/tatfook/py2lua
+
+## usage
+
+先将 Mod 安装到 paracraft 的搜索路径下
+
+```
+$ cd ${paracraft_installed_root}/Mod
+$ git clone https://github.com/tatfook/PyTranspiler
+```
+
+在代码中使用 PyTranspiler
+
+```lua
+local py2lua = NPL.load("Mod/PyTranspiler/Transpiler.lua")
+local lua_code = py2lua:transpile("b = 3")
+print(lua_code)
+```
+
+示例结果
+
+```
+local b = 3
+```
+
+## error handling
+
+在使用 mod 的过程中，可能会出现错误情况，比如
+- python 代码语法不正确
+- etc
+
+`transpile()`方法会返回两个值，当转换结果为 `nil` 时，说明转换出现问题，相关错误信息会保存在第 2 个值。
+
+```lua
+local lua_code, error_msg = py2lua:transpile("b = 3")
+if lua_code == nil then
+    -- handle error_msg
+end
+```
+
